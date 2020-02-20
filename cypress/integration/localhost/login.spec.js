1ddd6ce5bff7b894d1738fe7a3f827a86be0d9c2
ksahlq <khalid.sahlqeath@outlook.com>
2020-02-20 12:53:10 +0100

    changed API_ROOT from production to localhost

--- cypress/integration/localhost/login.spec.js
+++ cypress/integration/localhost/login.spec.js
@@ -1,10 +1,10 @@
-context('Login to page and store token', () => {
+context('Try to log in with a user that is not in database', () => {
     it('Visit the login page', () => {
         cy.visit('/login')
         cy.url().should('eq', Cypress.config().baseUrl + '/login')
     })
 
-    it('Login via UI', () => {
+    it('Try to login via UI with a user that does not exist ', () => {
         cy.visit('/login')
         .get('input[type="email"]')
         .type('ibrahim@login.login')
@@ -14,7 +14,7 @@ context('Login to page and store token', () => {
         .should('contain', 'invalid')
     })
 
-    it('Login using API', () => {                 
+    it('Try to login via API with user that does not exist', () => {                 
         cy.request({
             method: "POST",
             url: Cypress.env('apiUrl') + '/api/users/login',
